97f27560316f8f1ebe73e9aa8970c58b7966b5f2
Faria Jahan Janie <janiefaria315@gmail.com>
2026-04-09 01:47:09 +0600

    Add files via upload

---
+++ array.rb
@@ -0,0 +1,13 @@
+queue= []
+
+#push the array's data
+queue.push("A")
+queue.push("B")
+queue.push("C")
+queue.push("D")
+
+queue.shift()  #eleminate the 1st element
+queue.shift()  #eleminate the 2nd element
+puts queue
+
+#output: C D
